0081b30eae5fab801eb46cc7767a562063a8bc39
jackripttt <120683404+jackripttt@users.noreply.github.com>
2023-11-11 19:17:46 -0800

    Create DAY 1 - VARIABLES.py

---
+++ DAY 1 - VARIABLES.py
@@ -0,0 +1,19 @@
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+# There are two variables, a and b
+# Create a third variable to help switch the values
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
